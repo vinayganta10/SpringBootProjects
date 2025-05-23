e8643ba9164a4713b8a5fb921fa7049df4516fe6
vinayganta10 <aggvinayganta10@gmail.com>
2025-05-23 11:16:58 +0530

    basic product api setup

--- demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,8 +12,29 @@ public class ProductController {
 
     @Autowired
     ProductService service;
-    @RequestMapping("/product")
+    @GetMapping("/product")
     public List<Product> getProducts(){
         return service.getProductList();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProductById(@PathVariable int id){
+        return service.getProductById(id);
+    }
+
+    @PostMapping("/product")
+    public void addProduct(@RequestBody Product product){
+        service.addProduct(product);
+    }
+
+    @PutMapping("/product")
+    public void updateProduct(@RequestBody Product product){
+        service.updateProduct(product);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public String deleteProduct(@PathVariable int id){
+        service.deleteProduct(id);
+        return "Product deleted";
     }
 }
--- demo/src/main/java/com/example/demo/service/ProductService.java
+++ demo/src/main/java/com/example/demo/service/ProductService.java
@@ -8,9 +8,44 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private List<Product> productList= Arrays.asList(new Product(101,"OnePlus 12R",30000),new Product(102,"OnePlus NORD CE3",28000),new Product(103,"SAMSUNG S20",40000));
+    private List<Product> productList= new ArrayList<>(Arrays.asList(new Product(101,"OnePlus 12R",30000),new Product(102,"OnePlus NORD CE3",28000),new Product(103,"SAMSUNG S20",40000)));
 
     public List<Product> getProductList(){
         return productList;
+    }
+
+    public Product getProductById(int id) {
+        return productList.stream()
+                .filter(p->p.getProductId()==id)
+                .findFirst()
+                .orElse(new Product(0,"NO ITEM",0));
+    }
+
+
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+
+    public void updateProduct(Product product) {
+        int idx = -1;
+        for(int i=0;i<productList.size();i++){
+            if(product.getProductId()==productList.get(i).getProductId()){
+                idx=i;
+                break;
+            }
+        }
+        productList.set(idx,product);
+    }
+
+    public void deleteProduct(int id) {
+        int idx = -1;
+        for(int i=0;i<productList.size();i++){
+            if(id==productList.get(i).getProductId()){
+                idx=i;
+                break;
+            }
+        }
+        productList.remove(idx);
     }
 }
