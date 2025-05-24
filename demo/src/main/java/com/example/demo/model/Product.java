e10172666cfad8bad4e273c5fa0b731cd16a0d9e
vinayganta10 <aggvinayganta10@gmail.com>
2025-05-24 10:55:43 +0530

    Jpa data repository added

--- demo/src/main/java/com/example/demo/model/Product.java
+++ demo/src/main/java/com/example/demo/model/Product.java
@@ -1,12 +1,23 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
+@Component
+@Entity
 public class Product {
+
+    @Id
     private int productId;
     private String productName;
     private int productPrice;
+
+    public Product(){
+
+    }
 }
--- demo/src/main/java/com/example/demo/service/ProductService.java
+++ demo/src/main/java/com/example/demo/service/ProductService.java
@@ -4,48 +4,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private List<Product> productList= new ArrayList<>(Arrays.asList(new Product(101,"OnePlus 12R",30000),new Product(102,"OnePlus NORD CE3",28000),new Product(103,"SAMSUNG S20",40000)));
+    //private List<Product> productList= new ArrayList<>(Arrays.asList(new Product(101,"OnePlus 12R",30000),new Product(102,"OnePlus NORD CE3",28000),new Product(103,"SAMSUNG S20",40000)));
+
+    @Autowired
+    ProductRepo repo;
 
     public List<Product> getProductList(){
-        return productList;
+        return repo.findAll();
     }
 
     public Product getProductById(int id) {
-        return productList.stream()
-                .filter(p->p.getProductId()==id)
-                .findFirst()
-                .orElse(new Product(0,"NO ITEM",0));
+        return repo.findById(id).orElse(new Product(0,"No item",0));
     }
 
 
     public void addProduct(Product product) {
-        productList.add(product);
+        repo.save(product);
     }
 
 
     public void updateProduct(Product product) {
-        int idx = -1;
-        for(int i=0;i<productList.size();i++){
-            if(product.getProductId()==productList.get(i).getProductId()){
-                idx=i;
-                break;
-            }
-        }
-        productList.set(idx,product);
+        repo.save(product);
     }
 
     public void deleteProduct(int id) {
-        int idx = -1;
-        for(int i=0;i<productList.size();i++){
-            if(id==productList.get(i).getProductId()){
-                idx=i;
-                break;
-            }
-        }
-        productList.remove(idx);
+        repo.deleteById(id);
     }
 }
